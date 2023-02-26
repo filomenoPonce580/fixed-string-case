c7ef6afcbd8cc47d6887e67ce07ebf91f2543d5e
filomenoPonce580 <110742301+filomenoPonce580@users.noreply.github.com>
2023-02-25 23:30:50 -0800

    Create main.js

---
+++ main.js
@@ -0,0 +1,12 @@
+function solve(s){
+  //create uppes/lower variables
+  
+  
+  //iterate through string. if upper, +1, if lower, +1
+  
+  
+  //split, map(map letters to upper/lower depending on which number is greater)
+  
+  
+  //join and return
+}
