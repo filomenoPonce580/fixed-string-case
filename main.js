97ce6a0765c887ce9d250ea0a9852944d05e444d
filomenoPonce580 <110742301+filomenoPonce580@users.noreply.github.com>
2023-02-26 19:37:51 -0800

    Update main.js
    not working yet. get .includes to work or find another method

--- main.js
+++ main.js
@@ -1,12 +1,27 @@
 function solve(s){
   //create uppes/lower variables
-  
+  let uppers = 0;
+  let lowers = 0;
+  let sA = s.split('')
   
   //iterate through string. if upper, +1, if lower, +1
-  
+  for(let i = 0; i < sA.length; i++){
+    if(sA[i].includes(/[a-z]/g)){
+      lowers++
+    }else if(sA[i].includes(/[A-Z]/g)){
+      uppers++       
+    }
+  }
   
   //split, map(map letters to upper/lower depending on which number is greater)
-  
+  sA.split('').map((x,i,a)=>{
+    if(uppers > lowers){
+      x.toUpperCase()
+    }else if(uppers > lowers){
+      x.toLowerCase
+    }
+  })
   
   //join and return
+  return sA
 }
